81266e576f9851c42ffae16c8b6cab9dc8be3a95
Paulo Antiquera <paulo@antiquera.net>
2016-08-11 11:50:29 -0300

    Lab 3 done

--- lab3.js
+++ lab3.js
@@ -11,22 +11,25 @@ var port = process.env.port || 3000;
 var bucketName = "customer360";
 var password = "password";
 
-var customer360Bucket = cluster.openBucket(bucketName, password, function(error) {
-  if (error) {
+function openBucket(bucketName, password, callback) {
+  var bucket = cluster.openBucket(bucketName, password, callback);
+
+  return bucket;
+}
+
+function openBucketCallback(error) {
+  if (error) {  
     throw error;
   }
 
   console.log(bucketName, 'bucket opened');
-});
-
-// Set up get, post, put, and delete routes
-// Set jsonParser as middleware for POST and PUT routes, so that req.body can be accessed for JSON
-// Declare and assign doc and docId vars in each route, as relevant, with request data
-// Open bucket in each route
+}
 
 app.use(jsonParser);
 
 app.get("/customers/:id", function(request, response){
+  var customer360Bucket = openBucket(bucketName, password, openBucketCallback);
+
   var docId = request.params.id;
 
   customer360Bucket.get(docId, function(error, rows) {
@@ -36,7 +39,6 @@ app.get("/customers/:id", function(request, response){
 
     response.json(rows.value);
   })
-
 });
 
 app.post("/customers", jsonParser, function(request, response) {
@@ -57,6 +59,8 @@ app.post("/customers", jsonParser, function(request, response) {
     response.json(result);
   }
 
+  var customer360Bucket = openBucket(bucketName, password, openBucketCallback);
+
   var docIdSuffix = 'an-example-key';
   var docId = 'customer::' + docIdSuffix;
   var doc = request.body;
@@ -66,11 +70,32 @@ app.post("/customers", jsonParser, function(request, response) {
 });
 
 app.put("/customers/:id", jsonParser, function(request, response){
-  return response.status(400).json({'error' : 'Not implemented yet'});
+  var customer360Bucket = openBucket(bucketName, password, openBucketCallback);
+
+  var docId = request.params.id;
+  var doc = request.body;
+
+  customer360Bucket.replace(docId, doc, function(error, result) {
+    if (error) {
+      return response.status(400).json(error);
+    }
+
+    return response.json(result);
+  });
 });
 
 app.delete("/customers/:id", function(request, response){
-  return response.status(400).json({'error' : 'Not implemented yet'});
+  var customer360Bucket = openBucket(bucketName, password, openBucketCallback);
+
+  var docId = request.params.id;
+
+  customer360Bucket.remove(docId, function(error, result) {
+    if (error) {
+      return response.status(400).json(error);
+    }
+
+    return response.json(result);
+  });  
 });
 
 app.listen(port, function(err){
